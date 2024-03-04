7d225b2d6a4f36eb6106ba7ccfd2386785aa8713
ShrimpBúrger <96571916+ShrimpBurgerr@users.noreply.github.com>
2024-03-04 20:53:35 +0000

    Hello world

--- Documents/Builds/multilingual-chat-app/components/UserButton.tsx
+++ Documents/Builds/multilingual-chat-app/components/UserButton.tsx
@@ -11,30 +11,33 @@ import {
 import UserAvatar from "./UserAvatar"
 import { Session } from "next-auth";
 import { Button } from "./ui/button";
-import { signIn } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
 
    function UserButton({ session }: {session: Session | null}) {
+    // Subscription listener...
+    
   if (!session) return (
     <Button variant={'outline'} onClick={() => signIn()}>
       Sign In
     </Button>
-  )
+  );
 
-  return session && (
+  return (
+   session && (
   <DropdownMenu>
   <DropdownMenuTrigger>
-    <UserAvatar name={session.user?.name} 
-    image="https://github.com/shadcn.png" />
+    <UserAvatar name={session.user?.name} image={session.user?.image} />
   </DropdownMenuTrigger>
   <DropdownMenuContent>
-    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuLabel>{session.user?.name}</DropdownMenuLabel>
     <DropdownMenuSeparator />
-    <DropdownMenuItem>Profile</DropdownMenuItem>
-    <DropdownMenuItem>Billing</DropdownMenuItem>
-    <DropdownMenuItem>Team</DropdownMenuItem>
-    <DropdownMenuItem>Subscription</DropdownMenuItem>
+   
+
+    <DropdownMenuItem onClick={(() => signOut)}>Sign Out</DropdownMenuItem>
   </DropdownMenuContent>
 </DropdownMenu>
+
+   )
 ); 
 }
 
